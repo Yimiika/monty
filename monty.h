8e3b21d3889217025af38e8f9cd17e8a3a4cabea
Yimiika <oluwafemiaina8@gmail.com>
2023-08-24 05:38:27 -0700

    coding

--- monty.h
+++ monty.h
@@ -39,6 +39,7 @@ void pint(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
-void nop(stack_t **stack, unsigned int line_number)
+void nop(stack_t **stack, unsigned int line_number);
+int main(int argc, char *argv[]);
 
 #endif
