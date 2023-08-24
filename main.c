65beb6212af2433b73ffcfc73d674c5ab59c39d5
Yimiika <oluwafemiaina8@gmail.com>
2023-08-24 03:30:45 -0700

    coding

---
+++ main.c
@@ -0,0 +1,17 @@
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "USAGE: monty file\n");
+        return EXIT_FAILURE;
+    }
+
+    FILE *file = fopen(argv[1], "r");
+    if (!file) {
+        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+        return EXIT_FAILURE;
+    }
+
+    // Read and parse lines, execute opcode functions
+
+    fclose(file);
+    return EXIT_SUCCESS;
+}
